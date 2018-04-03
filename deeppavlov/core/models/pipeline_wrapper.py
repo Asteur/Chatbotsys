ffcde366ba62551d682d7bf3fa1bb19119a98924
Aleksey Lymar <yoptar@gmail.com>
2018-04-03 17:57:49 +0300

    feat: somewhat improved wrapper draft

--- deeppavlov/core/models/pipeline_wrapper.py
+++ deeppavlov/core/models/pipeline_wrapper.py
@@ -1,27 +1,69 @@
-from deeppavlov.core.common.chainer import Chainer
+from abc import ABCMeta, abstractmethod
+
+from deeppavlov.core.common.log import get_logger
 
 
-class Wrapper:
-    def __init__(self, *args):
-        self.chainer = Chainer()
-        self.ready = False
+log = get_logger(__name__)
 
+
+class BasicWrapper(metaclass=ABCMeta):
+    @abstractmethod
     def __call__(self, *args, **kwargs):
         pass
 
-    def fit(self, generate_batches, batch_size=1):
-        self._build_chainer(generate_batches=lambda: generate_batches(batch_size))
-
-    def _batch_pred_eval(self, batch):
+    @abstractmethod
+    def fit(self, generate_batches, batch_size=1, save_dir=None,
+            *args, **kwargs):
         pass
 
+    @abstractmethod
     def evaluate(self, batches_generator):
         pass
 
-    def load(self, path):
-        self._build_chainer(pretrained_path=path)
+    @abstractmethod
+    def load(self, load_dir):
+        pass
 
-    def _build_chainer(self, pretrained_path=None, generate_batches=None):
-        if pretrained_path is None and generate_batches is None:
-            raise RuntimeError('Have to have load path or data provider to build the pipeline')
-        self.ready = True
+    @abstractmethod
+    def save(self, save_dir):
+        pass
+
+
+class NNWrapper(BasicWrapper):
+    def __init__(self):
+        self.ready = False
+        self._epochs_done = 0
+        self._batches_seen = 0
+
+    def load(self, load_dir):
+        self._prepare_components(load_dir=load_dir)
+
+    def fit(self, generate_batches, batch_size=1, generate_valid_batches=None,
+            save_dir=None,
+            epochs_limit=-1, batches_limit=-1,
+            *args, **kwargs):
+        self._prepare_components(generate_batches=generate_batches)
+        should_stop = False
+        try:
+            while True:
+                for batch in generate_batches(batch_size=batch_size):
+                    self._train_on_batch(batch)
+                    self._batches_seen += 1
+                    if -1 < batches_limit <= self._batches_seen:
+                        should_stop = True
+                        break
+                if should_stop:
+                    break
+                self._epochs_done += 1
+                if -1 < epochs_limit <= self._epochs_done:
+                    break
+        except KeyboardInterrupt:
+            pass
+
+    @abstractmethod
+    def _prepare_components(self, load_dir=None, generate_batches=None, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def _train_on_batch(self, batch):
+        pass
