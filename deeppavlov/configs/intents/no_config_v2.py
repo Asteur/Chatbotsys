9c85eb024a1cb98d005fd671ee5be328e149d1f2
Aleksey Lymar <yoptar@gmail.com>
2018-04-05 10:49:55 +0300

    feat: add a draft for wrapped intents model

---
+++ deeppavlov/configs/intents/no_config_v2.py
@@ -0,0 +1,129 @@
+from pathlib import Path
+from typing import Iterable
+
+from deeppavlov.core.commands.train import _train_batches
+from deeppavlov.core.commands.utils import expand_path
+from deeppavlov.core.data.dataset_iterator import BasicDatasetIterator
+from deeppavlov.core.models.pipeline_wrapper import NNWrapper
+from deeppavlov.metrics.accuracy import sets_accuracy
+from deeppavlov.models.classifiers.intents.intent_model import KerasIntentModel
+from deeppavlov.models.embedders.fasttext_embedder import FasttextEmbedder
+from deeppavlov.models.tokenizers.nltk_tokenizer import NLTKTokenizer
+
+
+class IntentsNNWrapper(NNWrapper):
+    def __init__(self, emb_path: [Path, str], classes: Iterable=None,
+                 save_dir='intents/simple_classifier'):
+        super().__init__()
+
+        self.ft = FasttextEmbedder(load_path=emb_path)
+
+        self.classes = classes
+        self.save_path = expand_path(save_dir) / 'model'
+
+        self.model: KerasIntentModel = None
+        self.model_params = {
+          "kernel_sizes_cnn": [
+            1,
+            2,
+            3
+          ],
+          "filters_cnn": 256,
+          "lear_metrics": [
+            "binary_accuracy",
+            "fmeasure"
+          ],
+          "confident_threshold": 0.5,
+          "optimizer": "Adam",
+          "lear_rate": 0.01,
+          "lear_rate_decay": 0.1,
+          "loss": "binary_crossentropy",
+          "text_size": 15,
+          "coef_reg_cnn": 1e-4,
+          "coef_reg_den": 1e-4,
+          "dropout_rate": 0.5,
+          "epochs": 1000,
+          "dense_size": 100,
+          "model_name": "cnn_model"
+        }
+
+        # self.chainer = Chainer(in_x='text', in_y='classes', out_params='classes_predicted')
+
+    def _prepare_components(self, load_dir=None, generate_batches=None, batch_size=1, *args, **kwargs):
+        pass
+
+    def _train_on_batch(self, batch):
+        self.model.train_on_batch(*batch)
+
+    def evaluate(self, batches_iterator):
+        examples_len = 0
+        correct = 0
+        for x, y_true in batches_iterator:
+            examples_len += len(y_true)
+            y_predicted = self(x)
+            correct += sum([set(y1) == set(y2) for y1, y2 in zip(y_true, y_predicted)])
+
+        accuracy = correct / examples_len if examples_len > 0 else 0
+        return {'accuracy': accuracy}
+
+    def save(self, save_dir):
+        self.model.save()
+
+    def __call__(self, batch, predict_proba=False):
+        if not self.ready:
+            raise RuntimeError('Tried to run not fitted classifier. Fit or load it first')
+        return self.model(batch, predict_proba=predict_proba)
+
+    def predict(self, batch):
+        return self(batch)
+
+    def predict_proba(self, batch):
+        return self(batch, predict_proba=True)
+
+    def fit(self, x, y, val_x=None, val_y=None, batch_size=64, epochs=100, patience=5):
+        validate = val_x and val_y
+
+        if not self.classes:
+            self.classes = {label for labels in y for label in labels}
+
+        data = {
+            'train': list(zip(x, y))
+        }
+        if validate:
+            data['valid'] = list(zip(val_x, val_y))
+
+        del x, y, val_x, val_y
+
+        ds_iterator = BasicDatasetIterator(data)
+
+        self.model = KerasIntentModel(self.model_params, self.ft, NLTKTokenizer(), self.classes, mode='train',
+                                      save_path=self.save_path)
+
+        train_config = {
+            "epochs": epochs,
+            "batch_size": batch_size,
+            "validation_patience": patience,
+            "val_every_n_epochs": validate and 1 or 0,
+            "log_every_n_epochs": 1,
+            "validate_best": validate,
+            "test_best": False
+        }
+
+        _train_batches(self.model, ds_iterator, train_config, [('accuracy', sets_accuracy)])
+
+        self.model = KerasIntentModel(self.model_params, self.ft, NLTKTokenizer(), self.classes, mode='infer',
+                                      save_path=self.save_path, load_path=self.save_path)
+        self.classes = self.model.classes
+
+        self.ready = True
+
+    def load(self, load_path=None):
+        if load_path:
+            load_path = expand_path(load_path)
+        else:
+            load_path = self.save_path
+
+        self.model = KerasIntentModel(self.model_params, self.ft, NLTKTokenizer(), self.classes, mode='infer',
+                                      save_path=self.save_path, load_path=load_path)
+        self.classes = self.model.classes
+        self.ready = True
--- deeppavlov/core/models/pipeline_wrapper.py
+++ deeppavlov/core/models/pipeline_wrapper.py
@@ -1,7 +1,10 @@
+import datetime
+import itertools
+import json
+import time
 from abc import ABCMeta, abstractmethod
 
 from deeppavlov.core.common.log import get_logger
-
 
 log = get_logger(__name__)
 
@@ -17,7 +20,7 @@ class BasicWrapper(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def evaluate(self, batches_generator):
+    def evaluate(self, batches_iterator):
         pass
 
     @abstractmethod
@@ -38,15 +41,41 @@ class NNWrapper(BasicWrapper):
     def load(self, load_dir):
         self._prepare_components(load_dir=load_dir)
 
-    def fit(self, generate_batches, batch_size=1, generate_valid_batches=None,
-            save_dir=None,
+    def fit(self, generate_train_batches, batch_size=1,
+            generate_valid_batches=None,
+            save_dir=None, load_dir=None,
             epochs_limit=-1, batches_limit=-1,
+            log_every_n_batches=-1, log_every_n_epochs=-1, eval_on_n_train_batches=1,
+            validate_every_n_epochs=-1,
             *args, **kwargs):
-        self._prepare_components(generate_batches=generate_batches)
+        self._prepare_components(generate_batches=generate_train_batches, batch_size=batch_size, load_dir=load_dir)
+        if generate_valid_batches is None and validate_every_n_epochs > 0:
+            validate_every_n_epochs = -1
+            log.warn('Cannot validate if no generator function is provided for the valid data')
         should_stop = False
+        new_epoch = False
+        start_time = time.time()
         try:
             while True:
-                for batch in generate_batches(batch_size=batch_size):
+                for batch in generate_train_batches(batch_size=batch_size, shuffle=True):
+                    if new_epoch and 0 < log_every_n_epochs and self._epochs_done % log_every_n_epochs == 0\
+                            or 0 < log_every_n_batches and self._batches_seen > 0\
+                            and self._batches_seen % log_every_n_batches == 0:
+                        report = {
+                            'epochs_done': self._epochs_done,
+                            'batches_seen': self._batches_seen,
+                            'time_spent': str(datetime.timedelta(seconds=round(time.time() - start_time + 0.5)))
+                        }
+                        if eval_on_n_train_batches != 0:
+                            if eval_on_n_train_batches == 1:
+                                it = iter([batch])
+                            else:
+                                it = generate_train_batches(batch_size=batch_size, shuffle=True)
+                                if eval_on_n_train_batches > 0:
+                                    it = itertools.islice(it, eval_on_n_train_batches)
+                            report['metrics'] = self.evaluate(it)
+                        print(json.dumps(report, ensure_ascii=False))
+                    new_epoch = False
                     self._train_on_batch(batch)
                     self._batches_seen += 1
                     if -1 < batches_limit <= self._batches_seen:
@@ -55,13 +84,14 @@ class NNWrapper(BasicWrapper):
                 if should_stop:
                     break
                 self._epochs_done += 1
+                new_epoch = True
                 if -1 < epochs_limit <= self._epochs_done:
                     break
         except KeyboardInterrupt:
-            pass
+            log.info('Interrupted training')
 
     @abstractmethod
-    def _prepare_components(self, load_dir=None, generate_batches=None, *args, **kwargs):
+    def _prepare_components(self, load_dir=None, generate_batches=None, batch_size=1, *args, **kwargs):
         pass
 
     @abstractmethod
