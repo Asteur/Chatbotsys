03ec3bdfb9fcc945fe22b57e7689e196785292de
mu-arkhipov <arkhipov@yahoo.com>
2018-04-16 17:50:36 +0300

    refactor: vocab with prunned methods

--- deeppavlov/core/data/vocab.py
+++ deeppavlov/core/data/vocab.py
@@ -15,13 +15,12 @@ limitations under the License.
 """
 
 from collections import Counter, defaultdict
-import itertools
+from itertools import chain
 from pathlib import Path
 
 import numpy as np
 
 from deeppavlov.core.common.registry import register
-from deeppavlov.core.common.attributes import check_attr_true
 from deeppavlov.core.common.errors import ConfigError
 from deeppavlov.core.common.log import get_logger
 from deeppavlov.core.models.estimator import Estimator
@@ -31,53 +30,19 @@ log = get_logger(__name__)
 
 @register('default_vocab')
 class DefaultVocabulary(Estimator):
-    def __init__(self, save_path, load_path, level='token',
-                 special_tokens=tuple(), default_token=None,
-                 tokenizer=None, *args, **kwargs):
-
-        super().__init__(load_path=load_path,
-                         save_path=save_path,
-                         mode=kwargs['mode'])
-
+    def __init__(self,
+                 special_tokens=tuple(),
+                 max_tokens=2**30,
+                 min_count=1,
+                 *args,
+                 **kwargs):
+        super().__init__(**kwargs)
         self.special_tokens = special_tokens
-        self.default_token = default_token
-        self.preprocess_fn = self._build_preprocess_fn(level, tokenizer)
-
-        # TODO check via decorator
-        self.reset()
+        self._max_tokens = max_tokens
+        self._min_count = min_count
+        self.freqs = None
         if self.load_path:
             self.load()
-
-    @staticmethod
-    def _build_preprocess_fn(level, tokenizer=None):
-        def iter_level(utter):
-            if isinstance(utter, list) and utter and isinstance(utter[0], dict):
-                tokens = (u['text'] for u in utter)
-            elif isinstance(utter, dict):
-                tokens = [utter['text']]
-            elif isinstance(utter, list) and (not utter or isinstance(utter[0], str)):
-                tokens = utter
-            else:
-                tokens = [utter]
-
-            if tokenizer is not None:
-                tokens = tokenizer([' '.join(tokens)])[0]
-            tokens = filter(None, tokens)
-
-            if level == 'token':
-                yield from tokens
-            elif level == 'char':
-                for token in tokens:
-                    yield from token
-            else:
-                raise ValueError("level argument is either equal to `token`"
-                                 " or to `char`")
-
-        def preprocess_fn(data):
-            for d in data:
-                yield from iter_level(d)
-
-        return preprocess_fn
 
     def __getitem__(self, key):
         if isinstance(key, (int, np.integer)):
@@ -102,56 +67,22 @@ class DefaultVocabulary(Estimator):
     def items(self):
         return self.freqs.most_common()
 
-    def reset(self):
-        # default index is the position of default_token
-        if self.default_token is not None:
-            default_ind = self.special_tokens.index(self.default_token)
-        else:
-            default_ind = 0
-        self._t2i = defaultdict(lambda: default_ind)
-        self._i2t = dict()
-        self.freqs = Counter()
+    def fit(self, tokens):
+        self.freqs = Counter(chain(*tokens))
+        # toks_freqs = self.freqs.most_common()[:self.]
 
-        for i, token in enumerate(self.special_tokens):
-            self._t2i[token] = i
-            self._i2t[i] = token
-            self.freqs[token] += 0
-
-    def fit(self, *args):
-        self.reset()
-        self._train(
-            tokens=filter(None, itertools.chain.from_iterable(
-                map(self.preprocess_fn, zip(*args)))),
-            counts=None,
-            update=True
-        )
-
-    def _train(self, tokens, counts=None, update=True):
-        counts = counts or itertools.repeat(1)
-        if not update:
-            self.reset()
-
-        index = len(self.freqs)
-        for token, cnt in zip(tokens, counts):
-            if token not in self._t2i:
-                self._t2i[token] = index
-                self._i2t[index] = token
-                index += 1
-            self.freqs[token] += cnt
-
-    def __call__(self, samples, **kwargs):
-        return [self[s] for s in samples]
+    def __call__(self, token_batch, **kwargs):
+        return [self[s] for s in token_batch]
 
     def save(self):
         log.info("[saving vocabulary to {}]".format(self.save_path))
 
         with self.save_path.open('wt') as f:
-            for n in range(len(self._t2i)):
+            for n in range(len(self)):
                 token = self._i2t[n]
                 cnt = self.freqs[token]
                 f.write('{}\t{:d}\n'.format(token, cnt))
 
-    # @check_path_exists()
     def load(self):
         if self.load_path:
             if self.load_path.is_file():
@@ -168,32 +99,3 @@ class DefaultVocabulary(Estimator):
                         self.__class__.__name__))
         else:
             raise ConfigError("`load_path` for {} is not provided!".format(self))
-
-    def idx2tok(self, idx):
-        return self._i2t[idx]
-
-    def idxs2toks(self, idxs, filter_paddings=False):
-        toks = []
-        for idx in idxs:
-            if not filter_paddings or idx != self.tok2idx('<PAD>'):
-                toks.append(self._i2t[idx])
-        return toks
-
-    def tok2idx(self, tok):
-        return self._t2i[tok]
-
-    def toks2idxs(self, toks):
-        return [self._t2i[tok] for tok in toks]
-
-    def batch_toks2batch_idxs(self, b_toks):
-        max_len = max(len(toks) for toks in b_toks)
-        # Create array filled with paddings
-        batch = np.ones([len(b_toks), max_len]) * self.tok2idx('<PAD>')
-        for n, tokens in enumerate(b_toks):
-            idxs = self.toks2idxs(tokens)
-            batch[n, :len(idxs)] = idxs
-        return batch
-
-    def batch_idxs2batch_toks(self, b_idxs, filter_paddings=False):
-        return [self.idxs2toks(idxs, filter_paddings) for idxs in b_idxs]
-
